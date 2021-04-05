3b112573d871f0f72075b140d2ce632ff2e801af
willianguimaraes <willianfg2@gmail.com>
2021-04-05 13:46:04 -0300

    Criando e configurando serivico Usuario

--- ms-config-server/src/main/java/com/willian/msconfigserver/MsConfigServerApplication.java
+++ ms-config-server/src/main/java/com/willian/msconfigserver/MsConfigServerApplication.java
@@ -1,15 +1,27 @@
 package com.willian.msconfigserver;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class MsConfigServerApplication {
+public class MsConfigServerApplication implements CommandLineRunner {
+
+	@Value("${spring.cloud.config.server.git.username}")
+	private String user;
+
+	@Value("${spring.cloud.config.server.git.password}")
+	private String pass;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsConfigServerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("User " + user + " Pass " + pass);
+	}
 }
---
+++ ms-user/src/main/java/com/willian/msuser/MsUserApplication.java
@@ -0,0 +1,15 @@
+package com.willian.msuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class MsUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsUserApplication.class, args);
+	}
+
+}
